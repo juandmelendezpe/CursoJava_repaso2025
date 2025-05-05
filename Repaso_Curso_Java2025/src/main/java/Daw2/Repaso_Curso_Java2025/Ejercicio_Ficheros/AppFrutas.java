d97a1fbcdf9edfd48904a06e09b960abcaba2a57
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2025-05-05 12:58:53 +0200

    Update files map - ficheros
    Update files -- ficheros

--- Repaso_Curso_Java2025/src/main/java/Daw2/Repaso_Curso_Java2025/Ejercicio_Ficheros/AppFrutas.java
+++ Repaso_Curso_Java2025/src/main/java/Daw2/Repaso_Curso_Java2025/Ejercicio_Ficheros/AppFrutas.java
@@ -1,5 +1,6 @@
 package Daw2.Repaso_Curso_Java2025.Ejercicio_Ficheros;
 
+import java.awt.List;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -7,29 +8,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AppFrutas {
 
+	private static String[] NOMBRES_FRUTAS = {"fresas","mango","sandia","melon","uva"};
+	
+	private static char DELIMITADOR = ';';
+	
+	Random r = new Random();
+	
 	public static void main(String[] args) {
 		
 		
 		Frutas frutas = new Frutas();
+		
+		
 	
 		ArrayList<Frutas> lista = new ArrayList<>();
 		
-		lista.add(new Frutas("fresa",2.0));
-		lista.add(new Frutas("mango",5.0));
-		lista.add(new Frutas("fresa",8.0));
-		lista.add(new Frutas("sandia",9.0));
-		lista.add(new Frutas("melon",2.0));
+		/* lista.add(new Frutas("fresa",10,2.0));
+		lista.add(new Frutas("mango",20,5.0));
+		lista.add(new Frutas("fresa",8,8.0));
+		lista.add(new Frutas("sandia",9,9.0));
+		lista.add(new Frutas("melon",3,2.0));
+		*/
 		
 		guardarLista(lista);
 		
 
 	}
-	public static void guardarLista(ArrayList<Frutas>list) {
+	
+	/*private static List<Frutas> leerFicheros(String nombresFichros){
+		List<Frutas> frutas = new ArrayList<>();
+		
+		
+		
+	}
+	*/
+	
+	private static void generarFrutas() {
+		
+		//int numFile = r.nextInt(4)+2;
+		
+		
+		
+	}
+	private static void guardarLista(ArrayList<Frutas>list) {
+		
+		int numFicheros = 3 ;
 				
 		String linea1 = "**********Reportes de frutas**********" + "\n";
+		
+		
+			
 		
 		Path file = Paths.get("ficheros/ReporteFrutas.txt");
 		
@@ -51,7 +83,8 @@ public class AppFrutas {
 			System.err.println("  error al crear fichero externo...");
 
 		}
+		}
 		
 }
 	
-}
+
